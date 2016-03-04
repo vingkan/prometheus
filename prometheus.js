0593e6529cdddc2eea1796a3cf13aa3d165064e4
Vinesh Kannan <vkannan3@hawk.iit.edu>
2016-03-03 21:30:49 -0600

    Implemented basic Prometheus.save()

--- prometheus.js
+++ prometheus.js
@@ -1,3 +1,28 @@
+window.Prometheus = (function(){
+
+var GEOLOCATION = {
+	latitude: 0,
+	longitude: 0,
+	isValid: false
+};
+
+function updateCoords(position){
+	GEOLOCATION.latitude = position.coords.latitude;
+	GEOLOCATION.longitude = position.coords.longitude;
+	GEOLOCATION.isValid = true;
+}
+
+function getLocationData(){
+	var response = "NO_GEOLOCATION_EXCEPTION";
+	if(GEOLOCATION.isValid){
+		response = {
+			latitude: GEOLOCATION.latitude,
+			longitude: GEOLOCATION.longitude
+		}
+	}
+	return response;
+}
+
 function getDateTimeData(){
 	return {
 		timestamp: Date.now(),
@@ -23,20 +48,127 @@ function getBrowserData(){
 	}
 }
 
-window.Prometheus = (function(){
+function getPageData(){
+	return {
+		url: location.href
+	}
+}
 
 var Prometheus = function(config){
 
 	var URL = config.url || "NO_URL_EXCEPTION";
+	var LOCATOR = config.locator || false;
+
+	sessionStorage.setItem('prometheus_user', "NO_USER_TRACKING_EXCEPTION");
+
+	if(LOCATOR){
+		navigator.geolocation.getCurrentPosition(updateCoords);
+	}
 
 	var prometheus = {
+
+		getData: function(dataType){
+			var response;
+			switch(dataType){
+				case 'location':
+					response = getLocationData();
+					break;
+				case 'datetime':
+					response = getDateTimeData();
+					break;
+				case 'browser':
+					response = getBrowserData();
+					break;
+				case 'page':
+					response = getPageData();
+					break;
+				case 'all':
+					response = {
+						location: getLocationData(),
+						datetime: getDateTimeData(),
+						browser: getBrowserData(),
+						page: getPageData()
+					}
+					break;
+				default:
+					response = "BAD_REQUEST_EXCEPTION";
+			}
+			return response;
+		},
+
+		get: function(request){
+			var response = {};
+			if(Array.isArray(request) && request.length > 0){
+				if(request.includes('all')){
+					response = this.getData('all');
+				}
+				else{
+					for(var d = 0; d < request.length; d++){
+						var prop = request[d];
+						response[prop] = this.getData(prop);
+					}
+				}
+			}
+			else if(request){
+				response = this.getData(request);
+			}
+			else{
+				response = "BAD_REQUEST_EXCEPTION"
+			}
+			return response;
+		},
+
+		createRoute: function(endpoint){
+			var route;
+			if(URL !== "NO_URL_EXCEPTION"){
+				//TO-DO: check that endpoint has leading slash
+				route = new Firebase(URL + endpoint);
+			}
+			else{
+				throw new Error(URL, 'prometheus.js');
+			}
+			return route;
+		},
+
+		trackUser: function(uid){
+			sessionStorage.setItem('prometheus_user', uid);
+		},
+
+		getUID: function(){
+			var track = "ANONYMOUS_USER";
+			if(this.isTrackingUser()){
+				track = sessionStorage.getItem('prometheus_user');
+			}
+			return track;
+		},
+
+		save: function(dataObj, metaProps){
+			var uid = this.getUID();
+			var eventData = dataObj || "SAVED_VISIT";
+			var meta = metaProps || 'all';
+			var visitsRoute = this.createRoute('/users/' + uid + '/visits');
+			var payload = {
+				visit: this.get(meta),
+				data: eventData
+			}
+			console.log('POST to: ' + URL + '/users/' + uid + '/visits')
+			console.log(payload)
+			//visitsRoute.push();
+		},
+
+		isTrackingUser: function(){
+			var response = false;
+			var trackedUID = sessionStorage.getItem('prometheus_user');
+			if(trackedUID && trackedUID !== "NO_USER_TRACKING_EXCEPTION"){
+				response = true;
+			}
+			return response;
+		},
 
 		print: function(){
 			console.log('Bringing Firebase to humanity!');
 			console.log(config);
 		}
-
-
 
 	}
 
@@ -47,3 +179,9 @@ var Prometheus = function(config){
 return Prometheus;
 
 }());
+
+var prometheus = Prometheus({
+	//TO-DO: write check to see if '/prometheus' is at the end of url
+	url: 'https://prometheusjs.firebaseio.com/prometheus',
+	locator: true
+});
