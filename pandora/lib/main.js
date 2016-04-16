692fb8d64aeafb8a04eb398beeff7d9f2bf2c2e8
Vinesh Kannan <vkannan3@hawk.iit.edu>
2016-04-16 15:44:46 -0500

    Show user's last visit with moment

--- pandora/lib/main.js
+++ pandora/lib/main.js
@@ -1,25 +1,34 @@
 var LIVE = true;
 
 if(LIVE){
+	toggleLoading();
 	var fb = new Firebase('http://prometheusjs.firebaseio.com/prometheus');
-
 	fb.once('value', function(snapshot){
 		var data = snapshot.val();
 		main(data);
+		toggleLoading();
 	});
 }
 else{
 	main(FB_DATA.prometheus);
 }
 
+function toggleLoading(){
+	if(document.body.classList.contains('loading')){
+		document.body.classList.remove('loading');
+	}
+	else{
+		document.body.classList.add('loading');
+	}
+}
+
 function UserListDiv(user){
 	var img = user.profile.img || user.profile.picture;
 	var last = user.visits[user.visits.length-1];
-	var html = '<div class="user-list-div">';
+	var html = '<div class="user-list-div" id="user-list-div-' + user.id + '">';
 		html += '<div class="user-list-img" style="background-image: url(&quot;' + img + '&quot;);"></div>';
 		html += '<div class="user-list-name">' + user.profile.name + '</div>';
-		html += '<div class="user-list-info"><i class="fa fa-icon fa-eye"></i> ' + user.visits.length + '</div>';
-		html += '<div class="user-list-info"><i class="fa fa-icon fa-clock-o"></i> ' + moment(last.meta.datetime.timestamp).fromNow() + '</div>';
+		html += '<div class="user-list-info"><i class="fa fa-icon fa-eye"></i><span>' + user.visits.length + '</span><i class="fa fa-icon fa-clock-o"></i><span>' + moment(last.meta.datetime.timestamp).fromNow() + '</span></div>';
 		html += '</div>';
 	return html;
 }
@@ -36,6 +45,7 @@ function main(data){
 				visitList.push(user.visits[i]);
 			}
 			user.visits = visitList;
+			user.id = i;
 			userList.innerHTML += UserListDiv(user);
 			n++;
 		}
