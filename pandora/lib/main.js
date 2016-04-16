d9e3aa4973a7dad9f5e913fcde24fa5cedc84c56
Vinesh Kannan <vkannan3@hawk.iit.edu>
2016-04-16 15:05:05 -0500

    Basic user module to show number of visits and last visit

--- pandora/lib/main.js
+++ pandora/lib/main.js
@@ -1,60 +1,41 @@
-if (!String.prototype.supplant) {
-	String.prototype.supplant = function (o) {
-		return this.replace(
-			/\{([^{}]*)\}/g,
-			function (a, b) {
-				var r = o[b];
-				return typeof r === 'string' || typeof r === 'number' ? r : a;
-			}
-		);
-	};
+var LIVE = true;
+
+if(LIVE){
+	var fb = new Firebase('http://prometheusjs.firebaseio.com/prometheus');
+
+	fb.once('value', function(snapshot){
+		var data = snapshot.val();
+		main(data);
+	});
+}
+else{
+	main(FB_DATA.prometheus);
 }
 
-/*var fb = new Firebase('http://omnipointment.firebaseio.com/prometheus');
-
-fb.once('value', function(snapshot){
-	var data = snapshot.val();
-	main(data);
-});*/
-
-var userList = document.getElementById('user-list');
-
-main(FB_DATA.prometheus);
-
 function UserListDiv(user){
-	console.log(user)
-	var counter = {
-		total: 0,
-		login: 0,
-		error: 0,
-	};
-	for(var i in user.visits){
-		var visit = user.visits[i];
-		if(visit.type === 'USER_LOGON'){
-			counter.login++;
-		}
-		if(visit.type === 'ERROR'){
-			counter.error++;
-		}
-		counter.total++;
-	}
-	var html = '<div class="user-list-div">';
 	var img = user.profile.img || user.profile.picture;
-	html += '<div class="user-list-img" style="background-image: url(&quot;' + img + '&quot;);"></div>';
-	html += '<div class="user-list-name">{name}</div>'.supplant(user.profile);
-	html += '<div class="user-list-info info-total">{total}</div>'.supplant(counter);
-	html += '<div class="user-list-info info-login">{login}</div>'.supplant(counter);
-	html += '<div class="user-list-info info-error">{error}</div>'.supplant(counter);
-	html += '</div>';
+	var last = user.visits[user.visits.length-1];
+	var html = '<div class="user-list-div">';
+		html += '<div class="user-list-img" style="background-image: url(&quot;' + img + '&quot;);"></div>';
+		html += '<div class="user-list-name">' + user.profile.name + '</div>';
+		html += '<div class="user-list-info"><i class="fa fa-icon fa-eye"></i> ' + user.visits.length + '</div>';
+		html += '<div class="user-list-info"><i class="fa fa-icon fa-clock-o"></i> ' + moment(last.meta.datetime.timestamp).fromNow() + '</div>';
+		html += '</div>';
 	return html;
 }
 
 function main(data){
+	var userList = document.getElementById('user-list');
 	var max = 10;
 	var n = 0;
 	for(var i in data.users){
 		if(n < max){
 			var user = data.users[i];
+			var visitList = [];
+			for(var i in user.visits){
+				visitList.push(user.visits[i]);
+			}
+			user.visits = visitList;
 			userList.innerHTML += UserListDiv(user);
 			n++;
 		}
@@ -62,6 +43,5 @@ function main(data){
 			break;
 		}
 	}
-	console.log("DONE")
-	console.log(userList)
+	console.log("DONE");
 }
