0eaa21b7098b83f52c2ee63767471e84de840c4c
Vinesh Kannan <vkannan3@hawk.iit.edu>
2016-07-31 04:35:46 -0500

    Add simple dashboard

---
+++ pandora-simple/config.js
@@ -0,0 +1,6 @@
+window.CONFIG = {
+	apiKey: "AIzaSyAEv8mrdbxqO3efPHLrYW4B1LYrb-SDItQ",
+	authDomain: "prometheusjs-1240.firebaseapp.com",
+	databaseURL: "https://prometheusjs-1240.firebaseio.com",
+	storageBucket: "prometheusjs-1240.appspot.com"
+};
