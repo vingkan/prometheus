261d592d8b6d0be5055ab8033be6e9612f312285
Vinesh Kannan <vkannan3@hawk.iit.edu>
2016-10-13 13:16:24 -0500

    Simple Dashboard switched to visits branch

--- pandora-simple/new-lib/user-view-module.js
+++ pandora-simple/new-lib/user-view-module.js
@@ -5,28 +5,37 @@ window.UserViewModule = React.createClass({
 		return {
 			name: '',
 			uid: this.props.uid,
-			limit: 10
+			limit: 10,
+			visits: []
 		}
 	},
 	componentWillMount: function(){
-		var fb_url = 'prometheus/users/' + this.state.uid;
-		var ref = firebase.database().ref(fb_url);
-		var _this = this;
-		ref.on('value', function(snapshot){
-			var data = snapshot.val();
+		var stateContainer = this;
+		var visits_url = 'prometheus/visits/' + 'catherine';//this.state.uid;
+		var visitsRef = firebase.database().ref(visits_url);
+		visitsRef.on('value', function(visitsSnap){
+			
+			var visitsData = visitsSnap.val();
 			var visitList = [];
-			for(var i in data.visits){
-				visitList.push(data.visits[i]);
+			for(var i in visitsData){
+				visitList.push(visitsData[i]);
 			}
-			var email = data.profile.email || 'not listed';
-			_this.setState({
-				name: data.profile.name,
-				img: data.profile.img || data.profile.picture,
-				email: email,
-				visits: visitList
-			});
-			window.toggleLoading(false);
-		}).bind(this);
+
+			var fb_url = 'prometheus/users/' + 'catherine'//this.state.uid;
+			var ref = firebase.database().ref(fb_url);
+			ref.on('value', function(snapshot){
+				var data = snapshot.val();
+				var email = data.profile.email || 'not listed';
+				stateContainer.setState({
+					name: data.profile.name,
+					img: data.profile.img || data.profile.picture,
+					email: email,
+					visits: visitList
+				});
+				window.toggleLoading(false);
+			}).bind(this);
+
+		}).bind(stateContainer);
 	},
 	loadMore: function(){
 		this.setState(function(prev, curr){
@@ -36,91 +45,98 @@ window.UserViewModule = React.createClass({
 	render: function(){
 		var visits = _.clone(this.state.visits);
 		visits.reverse();
-		var visitList = visits.slice(0, this.state.limit);
-		var visitNodes = visitList.map(function(visit, index){
-			if(visit.visit.type === "TIMER"){
-				var timer = visit.visit;
-				var length = moment.duration(moment(timer.end).diff(timer.start));
-				var duration = '';
-				if(length.asMinutes() < 1){
-					duration = length.asSeconds() + ' seconds';
+		if(visits.length > 0){
+			var visitList = visits.slice(0, this.state.limit);
+			var visitNodes = visitList.map(function(visit, index){
+				if(visit.visit.type === "TIMER"){
+					var timer = visit.visit;
+					var length = moment.duration(moment(timer.end).diff(timer.start));
+					var duration = '';
+					if(length.asMinutes() < 1){
+						duration = length.asSeconds() + ' seconds';
+					}
+					else{
+						duration = length.asMinutes() + ' minutes';
+					}
+					visit.visit.duration = duration;
 				}
-				else{
-					duration = length.asMinutes() + ' minutes';
-				}
-				visit.visit.duration = duration;
-			}
-			return (
-				<VisitView
-					meta={visit.meta} 
-					data={visit.visit} 
-					key={index}>
-				</VisitView>
-			);
-		});
-		if(this.state.visits.length > this.state.limit){
-			return (
-				<div className="UserViewModule user-view">
-					<h1>{this.state.name || 'No Name Listed'}</h1>
-					<p>
-						<i className="fa fa-icon fa-clock-o"></i>
-						User since {moment(visits[visits.length-1].meta.datetime.timestamp).format('M/D/YYYY')}
-					</p>
-					<p>
-						<i className="fa fa-icon fa-eye"></i>
-						{visits.length} total visits
-					</p>
-					<p>
-						<i className="fa fa-icon fa-envelope-o"></i>
-						{this.state.email || 'no email listed'}
-					</p>
-					<p>
-						<i className="fa fa-icon fa-code-fork"></i>
-						UID: {this.state.uid}
-					</p>
-					<div className="user-view-img" style={{
-						backgroundImage: 'url(' + (this.state.img || '"/style/img/faded-logo.png"') + ')'
-					}}></div>
-					<div className="visits-field">
-						{visitNodes}
+				return (
+					<VisitView
+						meta={visit.meta} 
+						data={visit.visit} 
+						key={index}>
+					</VisitView>
+				);
+			});
+			if(this.state.visits.length > this.state.limit){
+				return (
+					<div className="UserViewModule user-view">
+						<h1>{this.state.name || 'No Name Listed'}</h1>
 						<p>
-							Showing {this.state.limit}/{this.state.visits.length} visits.
+							<i className="fa fa-icon fa-clock-o"></i>
+							User since {moment(visits[visits.length-1].meta.datetime.timestamp).format('M/D/YYYY') || 'Unknown Date'}
 						</p>
-						<button class="load-more" onClick={this.loadMore}>
-							Load More Visits
-						</button>
+						<p>
+							<i className="fa fa-icon fa-eye"></i>
+							{visits.length} total visits
+						</p>
+						<p>
+							<i className="fa fa-icon fa-envelope-o"></i>
+							{this.state.email || 'no email listed'}
+						</p>
+						<p>
+							<i className="fa fa-icon fa-code-fork"></i>
+							UID: {this.state.uid}
+						</p>
+						<div className="user-view-img" style={{
+							backgroundImage: 'url(' + (this.state.img || '"/style/img/faded-logo.png"') + ')'
+						}}></div>
+						<div className="visits-field">
+							{visitNodes}
+							<p>
+								Showing {this.state.limit}/{this.state.visits.length} visits.
+							</p>
+							<button class="load-more" onClick={this.loadMore}>
+								Load More Visits
+							</button>
+						</div>
 					</div>
-				</div>
-			);
+				);
+			}
+			else{
+				return (
+					<div className="UserViewModule user-view">
+						<h1>{this.state.name}</h1>
+						<p>
+							<i className="fa fa-icon fa-clock-o"></i>
+							User since {moment(visits[visits.length-1].meta.datetime.timestamp).format('M/D/YYYY') || 'Unknown Date'}
+						</p>
+						<p>
+							<i className="fa fa-icon fa-eye"></i>
+							{visits.length} total visits
+						</p>
+						<p>
+							<i className="fa fa-icon fa-envelope-o"></i>
+							{this.state.email || 'no email listed'}
+						</p>
+						<p>
+							<i className="fa fa-icon fa-code-fork"></i>
+							UID: {this.state.uid}
+						</p>
+						<div className="user-view-img" style={{
+							backgroundImage: 'url(' + this.state.img + ')'
+						}}></div>
+						<div className="visits-field">
+							{visitNodes}
+							<p>End of History</p>
+						</div>
+					</div>
+				);
+			}
 		}
 		else{
 			return (
-				<div className="UserViewModule user-view">
-					<h1>{this.state.name}</h1>
-					<p>
-						<i className="fa fa-icon fa-clock-o"></i>
-						User since {moment(visits[visits.length-1].meta.datetime.timestamp).format('M/D/YYYY')}
-					</p>
-					<p>
-						<i className="fa fa-icon fa-eye"></i>
-						{visits.length} total visits
-					</p>
-					<p>
-						<i className="fa fa-icon fa-envelope-o"></i>
-						{this.state.email || 'no email listed'}
-					</p>
-					<p>
-						<i className="fa fa-icon fa-code-fork"></i>
-						UID: {this.state.uid}
-					</p>
-					<div className="user-view-img" style={{
-						backgroundImage: 'url(' + this.state.img + ')'
-					}}></div>
-					<div className="visits-field">
-						{visitNodes}
-						<p>End of History</p>
-					</div>
-				</div>
+				<p>No User Data</p>
 			);
 		}
 	}
