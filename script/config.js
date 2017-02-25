6fe8c90d274ed610da2f838e5d8211f117ee85ff
Vinesh Kannan <vingkan@gmail.com>
2017-02-24 23:10:52 -0600

    Update config to new Firebase

--- script/config.js
+++ script/config.js
@@ -1,8 +1,9 @@
 var prometheus = Prometheus({
-	apiKey: "AIzaSyAEv8mrdbxqO3efPHLrYW4B1LYrb-SDItQ",
-	authDomain: "prometheusjs-1240.firebaseapp.com",
-	databaseURL: "https://prometheusjs-1240.firebaseio.com",
-	storageBucket: "prometheusjs-1240.appspot.com",
+    	apiKey: "AIzaSyBXXQFcl6qtakmkFeh0jzy_jjjIDpb1DlY",
+    	authDomain: "prometheusjs.firebaseapp.com",
+    	databaseURL: "https://prometheusjs.firebaseio.com",
+    	storageBucket: "firebase-prometheusjs.appspot.com",
+    	messagingSenderId: "433905102741",
 	noScreenshots: true,
 	localhost: true
-});
+}});
